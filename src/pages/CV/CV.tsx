bdb9f54fa3aaa9a4b67db78176cf7d4092bb0c47
ethaqnix <romain.denizot@epitech.eu>
2024-02-16 18:17:46 +0100

    styles: improve responsivity

--- src/pages/CV/CV.tsx
+++ src/pages/CV/CV.tsx
@@ -6,28 +6,30 @@ import ProfilePicture from '@components/ProfilePicture/ProfilePicture'
 const CV = () => {
     return (
         <div className={styles.root}>
-            <div className={styles.picture}>
+            <section className={styles.picture}>
                 <ProfilePicture />
-            </div>
-            <div className={styles.profile}>
-                <div className={styles.contacts}>
+            </section>
+            <section className={styles.profile}>
+                <section className={styles.contacts}>
                     <h3>contacts :</h3>
                     <p>email: romain.denizot.pro@gmail.com</p>
                     <p>phone: 06 95 94 65 09</p>
-                </div>
-                <div className={styles.langages}>
+                </section>
+                <section className={styles.langages}>
                     <h3>langues :</h3>
                     <p>Anglais: Parlé</p>
                     <p>Français: Natif</p>
-                </div>
-                <div className={styles.interests}>
+                </section>
+                <section className={styles.interests}>
                     <h3>centre d'intéret :</h3>
                     <p>Echecs</p>
                     <p>Musique: {'<icon with link>'}</p>
-                </div>
+                </section>
                 <div className={styles.social}>github - linkedin</div>
-            </div>
-            <History />
+            </section>
+            <section className={styles.history}>
+                <History />
+            </section>
         </div>
     )
 }
