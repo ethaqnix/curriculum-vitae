4da97939682967c4efa6cb81421afbfa0ca1b781
ethaqnix <romain.denizot@epitech.eu>
2024-02-18 17:34:25 +0100

    utilty: add Template Folder in project

---
+++ .fttemplates/typescriptFolder/[FTName].tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './[FTName].module.scss'
+
+interface <FTName | capitalize>Props {}
+
+const <FTName | capitalize> = (props: <FTName | capitalize>Props) => {
+  return <div className={styles.root}></div>;
+};
+
+export default <FTName | capitalize>;
