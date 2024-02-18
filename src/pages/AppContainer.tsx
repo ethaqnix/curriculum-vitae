bbb7585048cff41506bd336f816029e9a0526235
ethaqnix <romain.denizot@epitech.eu>
2024-02-18 20:53:18 +0100

    feat: add mobile warning

--- src/pages/AppContainer.tsx
+++ src/pages/AppContainer.tsx
@@ -1,10 +1,19 @@
 import Menu from '@components/Menu'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './AppContainer.module.scss'
 import classNames from 'classnames'
 import CV from './CV'
+import useScreenSize from '@hooks/useScreenSize'
 
 const AppContainer = () => {
+    const { width } = useScreenSize()
+    const [showMobileWarning, setShowMobileWarning] = useState(false)
+
+    useEffect(() => {
+        if (width <= 800) setShowMobileWarning(true)
+        if (width > 800) setShowMobileWarning(false)
+    }, [width])
+
     return (
         <div
             className={classNames({
@@ -14,7 +23,14 @@ const AppContainer = () => {
         >
             <Menu />
             <div className={styles['app']}>
-                <CV />
+                {showMobileWarning ? (
+                    <div>
+                        Sorry, this website is only available on desktop devices
+                        for now.
+                    </div>
+                ) : (
+                    <CV />
+                )}
             </div>
         </div>
     )
