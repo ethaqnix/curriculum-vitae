7130335762f618e17ac949b0f3392eb7cd75a978
ethaqnix <romain.denizot@epitech.eu>
2024-02-20 18:19:19 +0100

    feat: writting french experiences-formations details - harder than dev

--- src/pages/CV/History/formations.ts
+++ src/pages/CV/History/formations.ts
@@ -4,13 +4,13 @@ export const formations: HistoryItem[] = [
     {
         title: 'BTS Iris',
         src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLShW2szCRi93uxALGN_ktXJ1vFaE3yhnGpw&usqp=CAU',
-        content: 'kekw',
+        content: 'iris',
         skills: [],
         software: [],
     },
     {
         title: 'Epitech',
-        content: 'bas voilà',
+        content: 'epitech',
         src: 'https://capsup.cio-montlucon.fr/wp-content/uploads/2016/10/th-1.jpg',
         skills: [],
         software: [],
@@ -18,7 +18,7 @@ export const formations: HistoryItem[] = [
     {
         src: 'https://mlusk6kjzisu.i.optimole.com/cb:Zy_l~5b968/w:auto/h:auto/q:mauto/id:b7bedccfc74ecbc80aabf3ebcbba3b9c/https://logo-creation.com/PFE_Icon_Public-education.png',
         title: 'Formations',
-        content: "Ce que j'ai fais solo",
+        content: 'selfTraining',
         skills: [],
         software: [],
     },
