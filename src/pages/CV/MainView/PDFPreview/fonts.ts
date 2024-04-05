d132b678f77f18865d3599302ba224f80a6ea016
ethaqnix <romain.denizot@epitech.eu>
2024-04-05 16:28:59 +0200

    fix: load fonts locally

---
+++ src/pages/CV/MainView/PDFPreview/fonts.ts
@@ -0,0 +1,20 @@
+export const fonts: { [key: string]: any } = {
+    Poppins: [
+        {
+            src: `${window.location.origin}${window.location.pathname}fonts/TDTjCH39JjVycIF24TlO-Q.ttf`,
+        },
+    ],
+    Roboto: [
+        {
+            src: `${window.location.origin}${window.location.pathname}fonts/W5F8_SL0XFawnjxHGsZjJA.ttf`,
+        },
+        {
+            src: `${window.location.origin}${window.location.pathname}fonts/bdHGHleUa-ndQCOrdpfxfw.ttf`,
+            fontStyle: 'bold',
+        },
+        {
+            src: `${window.location.origin}${window.location.pathname}fonts/dtpHsbgPEm2lVWciJZ0P-A.ttf`,
+            fontWeight: 'light',
+        },
+    ],
+}
