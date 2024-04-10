b6d0d868485c039fe0d6bd7a571f47d78104a9de
ethaqnix <romain.denizot@epitech.eu>
2024-04-10 19:19:13 +0200

    temp: comment eslint warning

--- src/pages/CV/MainView/MusicPreview/MusicPreview.tsx
+++ src/pages/CV/MainView/MusicPreview/MusicPreview.tsx
@@ -1,44 +1,45 @@
-import React from 'react'
-import styles from './MusicPreview.module.scss'
-import { CloseIcon } from '@components/Icons'
-import { Navigate, useLocation, useNavigate } from 'react-router-dom'
+import React from "react";
+import styles from "./MusicPreview.module.scss";
+import { CloseIcon } from "@components/Icons";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 const MusicPreview = () => {
-    const navigate = useNavigate()
-    const { state } = useLocation()
+  const navigate = useNavigate();
+  const { state } = useLocation();
 
-    if (!state) {
-        return <Navigate to="/curriculum-vitae/" replace={true} />
-    }
+  if (!state) {
+    return <Navigate to="/curriculum-vitae/" replace={true} />;
+  }
 
-    const { title, musicUrl } = state
+  const { title, musicUrl } = state;
 
-    const width = 450
+  const width = 450;
 
-    return (
-        <div className={styles.root}>
-            <CloseIcon
-                onClick={() => {
-                    navigate(
-                        new URL('..', window.origin + location.pathname + '/')
-                    )
-                }}
-            />
-            <h2>{title}</h2>
-            <div className={styles.player}>
-                <iframe
-                    className={title === 'Bonus' ? styles.hideTitle : ''}
-                    width={width}
-                    height={(width / 16) * 9}
-                    src={musicUrl}
-                    title={title}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen
-                ></iframe>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.root}>
+      <CloseIcon
+        onClick={() => {
+          navigate(
+            // eslint-disable-next-line no-restricted-globals
+            new URL("..", window.origin + location.pathname + "/")
+          );
+        }}
+      />
+      <h2>{title}</h2>
+      <div className={styles.player}>
+        <iframe
+          className={title === "Bonus" ? styles.hideTitle : ""}
+          width={width}
+          height={(width / 16) * 9}
+          src={musicUrl}
+          title={title}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+};
 
-export default MusicPreview
+export default MusicPreview;
