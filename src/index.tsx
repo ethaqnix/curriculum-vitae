97231636c83719188567528cdca97679d004867f
ethaqnix <romain.denizot@epitech.eu>
2024-04-11 17:40:36 +0200

    try deploying hello site

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const router = createBrowserRouter([
+/*const router = createBrowserRouter([
   {
     path: "/curriculum-vitae/*",
     element: <App />,
   },
-]);
+]);*/
 
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode>HELLO</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
